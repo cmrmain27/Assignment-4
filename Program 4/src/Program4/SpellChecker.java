03d4815fe138444ea538813fe726c422db9bdd24
cmrmain27 <cmrmain27@aol.com>
2016-10-21 13:25:03 -0400

    CM
    Initial Commit

--- Program 4/src/Program4/SpellChecker.java
+++ Program 4/src/Program4/SpellChecker.java
@@ -3,11 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Program4;
 
 /**
  *
  * @author cmrma_000
  */
 public class SpellChecker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
